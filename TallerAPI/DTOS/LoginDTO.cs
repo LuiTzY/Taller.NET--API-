9fd3b7e925d57b996eabf3d47a0b1e641cb4a2b3
Luis Marte <luisdavidmarte00@gmail.com>
2024-11-24 21:13:20 -0400

    Added Data Anottations for users login and registers

---
+++ TallerAPI/DTOS/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TallerAPI.DTOS
+{
+    public class LoginDTO
+    {  
+            public string Username { get; set; } = null!;
+            public string Password { get; set; } = null!;
+    }
+}
---
+++ TallerAPI/DTOS/UserRegisterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TallerAPI.DTOS
+{
+    public class UserRegisterDTO
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+    }
+}
