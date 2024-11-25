711ac1fde104ea0ee5bf032026e8224310cd13ac
Luis Marte <luisdavidmarte00@gmail.com>
2024-11-24 21:17:01 -0400

    Added the JWT services like an auth service, add some services scopes for the controllers that required

--- TallerAPI/Program.cs
+++ TallerAPI/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using Taller.infraestructure;
 using Taller.infraestructure.Repositories;
 using Taller.infraestructure.interfaces;
+using Taller.infraestructure.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TallerAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,13 +17,37 @@ builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
 builder.Services.AddScoped<IEmpleadoRepository, EmpleadoRepository>();
 builder.Services.AddScoped<IHistorialMantenimientoRepository, HistorialMantenimientoRepository>();
 builder.Services.AddScoped<IVehiculoRepository, VehiculoRepository>();
-
+builder.Services.AddScoped<IOrdenesTrabajoRepository, OrdenesTrabajoRepository>();
+builder.Services.AddScoped<IPiezaRepository, PiezaRepository>();
+builder.Services.AddScoped<IOrdenPiezaRepository, OrdenPiezaRepository>();
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
+builder.Services.AddScoped<AuthService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TallerVehiculosContext>(options =>
        options.UseSqlServer(builder.Configuration.GetConnectionString("TallerBdContext")));
 
+
+//Configuracion para la autenticacion con JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 var app = builder.Build();
 
 
